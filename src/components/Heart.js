ed7539c0c84e324a8e65ad3a6e40273f2f6b28fa
pisanzapra <pisanzapra@users.noreply.github.com>
2022-08-07 12:36:36 +0100

    Fix: clicking save repeatedly no longer highlights text

--- src/components/Heart.js
+++ src/components/Heart.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const Heart = (props) => {
   return (
-    <i
-      className="bi bi-heart-fill display-1"
-      onClick={() => props.addToShortlistHandler(props.colour, props.hue)}
-      title="Save this colour"
-    ></i>
+    <div>
+      <i
+        className="bi bi-heart-fill display-1"
+        onClick={() => props.addToShortlistHandler(props.colour, props.hue)}
+        title="Save this colour"
+      ></i>
+    </div>
   );
 };
 
